f94e94eda9be0e92ca66dffeb1d45e53b3397ff7
Rongjian Zhang <pd4d10@gmail.com>
2018-03-02 17:39:22 +0800

    Integrate router

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,7 +1,8 @@
 import 'package:angular/angular.dart';
-import 'package:angular_components/angular_components.dart';
+import 'package:angular_router/angular_router.dart';
+// import 'package:angular_components/angular_components.dart';
 
-import 'src/todo_list/todo_list_component.dart';
+import 'src/list_component.dart';
 
 // AngularDart info: https://webdev.dartlang.org/angular
 // Components info: https://webdev.dartlang.org/components
@@ -10,8 +11,18 @@ import 'src/todo_list/todo_list_component.dart';
   selector: 'my-app',
   styleUrls: const ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: const [materialDirectives, TodoListComponent],
-  providers: const [materialProviders],
+  directives: const [ROUTER_DIRECTIVES, CORE_DIRECTIVES],
+  providers: const [ROUTER_PROVIDERS],
+  // directives: const [materialDirectives],
+  // providers: const [materialProviders],
 )
+@RouteConfig(const [
+  const Route(path: '/top', name: 'Top', component: ListComponent),
+  const Route(path: '/new', name: 'New', component: ListComponent),
+  const Route(path: '/show', name: 'Show', component: ListComponent),
+  const Route(path: '/ask', name: 'Ask', component: ListComponent),
+  const Route(path: '/jobs', name: 'Jobs', component: ListComponent),
+])
 class AppComponent {
+  final tabs = ['Top', 'New', 'Show', 'Ask', 'Jobs'];
 }
--- lib/src/list_component.dart
+++ lib/src/list_component.dart
@@ -1,23 +1,22 @@
 import 'dart:async';
 
 import 'package:angular/angular.dart';
-import 'package:angular_components/angular_components.dart';
+// import 'package:angular_components/angular_components.dart';
 import 'package:firebase/firebase.dart';
 
-import 'todo_list_service.dart';
+import 'list_service.dart';
 
 @Component(
-  selector: 'todo-list',
-  styleUrls: const ['todo_list_component.css'],
-  templateUrl: 'todo_list_component.html',
+  selector: 'list',
+  styleUrls: const ['list_component.css'],
+  templateUrl: 'list_component.html',
   directives: const [
     CORE_DIRECTIVES,
-    materialDirectives,
+    // materialDirectives,
   ],
   providers: const [TodoListService],
 )
-class TodoListComponent implements OnInit {
-  final tabs = ['top', 'new', 'show', 'ask', 'jobs'];
+class ListComponent implements OnInit {
   List<int> items = [];
   DatabaseReference ref;
   Map<int, dynamic> stories = {};

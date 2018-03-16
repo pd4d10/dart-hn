72612a79ad10e55153b85fc4f37eb194be068b6b
Rongjian Zhang <pd4d10@gmail.com>
2018-03-16 12:46:21 +0800

    Integrate PWA

--- web/main.dart
+++ web/main.dart
@@ -1,9 +1,11 @@
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
 import 'package:dart_hn/app_component.dart';
+import 'package:pwa/client.dart' as pwa;
 
 void main() {
   bootstrap(AppComponent, [
+    new Provider(pwa.Client, useValue: new pwa.Client()),
     ROUTER_PROVIDERS,
     // Remove next line in production
     provide(LocationStrategy, useClass: HashLocationStrategy),
